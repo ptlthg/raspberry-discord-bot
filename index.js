a1e429355b713cdbd6184decab97836bfc8a3185
Kaeso <edmier12@gmail.com>
2019-07-20 17:03:19 -0500

    Typo

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on('message', message => {
 		client.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+		message.reply('There was an error trying to execute that command!');
 	}
 });
 
