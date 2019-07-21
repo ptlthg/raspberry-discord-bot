3e0ec162a5b166518393b38b5388941e59976379
Kaeso <edmier12@gmail.com>
2019-07-21 16:59:38 -0500

    Kill

--- commands/kill.js
+++ commands/kill.js
@@ -6,13 +6,13 @@ module.exports = {
 		let member = message.mentions.members.first();
 		if (member !== undefined) {
 			const { registerFont, createCanvas } = require('canvas');
-			registerFont('../fonts/Font-On-A-Stick.ttf', { family: 'Handwritten' });
+			registerFont('fonts/Font-On-A-Stick.ttf', { family: 'Handwritten' });
 
 			const canvas = createCanvas(500, 500);
 			const ctx = canvas.getContext('2d');
 			ctx.font = '12px "Handwritten"'
 			ctx.fillText('Down ' + member.displayName, 250, 10)
-			const background = Canvas.loadImage('../images/kill.jpg');
+			const background = Canvas.loadImage('images/kill.jpg');
 			// This uses the canvas dimensions to stretch the image onto the entire canvas
 			ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 			// Use helpful Attachment class structure to process the file for you
