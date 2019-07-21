81b89230fb066e7c2ad7ad864849f1b6ed622be1
Kaeso <edmier12@gmail.com>
2019-07-21 18:11:36 -0500

    Fix Text Resize

--- commands/kill.js
+++ commands/kill.js
@@ -18,7 +18,7 @@ module.exports = {
 				ctx.fillStyle = '#6b0c19';
 
 				ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-				if (ctx.measureText(member.displayName.toUpperCase()).width > canvas.width - 50) {
+				if ((ctx.measureText(member.displayName.toUpperCase()).width + (ctx.measureText(member.displayName.toUpperCase()).width / 2)) > canvas.width - 50) {
 					var fontsize = 128;
 					do {
 						fontsize--;
