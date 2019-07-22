2c2595ad14f522b82932d240012be918cf17ce44
Kaeso <edmier12@gmail.com>
2019-07-22 01:19:28 -0500

    Help

--- commands/help.js
+++ commands/help.js
@@ -29,24 +29,24 @@ module.exports = {
 				*/
 			}
 
-			if (args[0] === null) return;
-			const name = args[0].toLowerCase().catch(error => {
-				console.log('Help')
-			});
-			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+			if (args[0] !== null) {
+				const name = args[0].toLowerCase().catch(error => {
+					console.log('Help')
+				});
+				const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
-			if (!command) {
-				return message.reply('that\'s not a valid command!');
+				if (!command) {
+					return message.reply('that\'s not a valid command!');
+				}
+
+				data.push(`**Name:** ${command.name}`);
+
+				if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+				if (command.description) data.push(`**Description:** ${command.description}`);
+				if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+				//data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 			}
-
-			data.push(`**Name:** ${command.name}`);
-
-			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-			if (command.description) data.push(`**Description:** ${command.description}`);
-			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-			//data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
 			message.channel.send(data, { split: true });
 	},
 };
