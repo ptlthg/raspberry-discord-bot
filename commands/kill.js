e9aa5c790c23b55252b3a8c4b9ea8f513011d165
Kaeso <edmier12@gmail.com>
2019-07-22 03:18:28 -0500

    Kill v3

--- commands/kill.js
+++ commands/kill.js
@@ -24,57 +24,46 @@ module.exports = {
 			const { registerFont, createCanvas } = require('canvas');
 			registerFont('commands/fonts/Font-On-A-Stick.ttf', { family: 'Handwritten' });
 
-			if (state) {
-				let victims = JSON.parse(fs.readFile('commands/victims.json', 'utf8'));
-				if (!victim[member.id]) victims[member.id] = {
-					killed: 0,
-				};
-				let userData = victims[member.id];
-				userData.killed++;
-				fs.writeFile('commands/victims.json', JSON.stringify(killed), (err) => {
-					if (err) console.error(err);
-				});
-				if (userData.killed > 0) {
-					var random = Math.random();
-					if (random < 1) { //Change to 0.1
-						proceed = false
+			var random = Math.random();
+			if (random < 1) { //Change to 0.1
+				proceed = false
 
-						const background = await Canvas.loadImage('commands/images/killagain.jpg');
-						const canvas = createCanvas(background.width, background.height);
-						const ctx = canvas.getContext('2d');
+				const background = await Canvas.loadImage('commands/images/killagain.jpg');
+				const canvas = createCanvas(background.width, background.height);
+				const ctx = canvas.getContext('2d');
 
-						ctx.font = '128px "Handwritten"';
-						ctx.fillStyle = '#02a166';
+				ctx.font = '128px "Handwritten"';
+				ctx.fillStyle = '#02a166';
 
-						ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+				ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-						if (!state) {
-							var name = member.displayName.toUpperCase();
-							var nameWidth = ctx.measureText(name).width;
-							function fitText(text, size, fontface, fontsize, xPos, yPos) {
-								do {
-									fontsize--;
-									ctx.font = fontsize + 'px ' + fontface;
-								} while (ctx.measureText(text).width > size);
-								ctx.fillText(text, xPos, yPos);
-							}
-							if (nameWidth > canvas.width / 2 - 100) {
-								fitText(name, canvas.width / 2 - 100, 'Handwritten', 128, 50, canvas.height * 0.66);
-							} else {
-								ctx.fillText(name, canvas.width / 2 + 50, canvas.height * 0.66);
-							};
-						} else {
-							ctx.beginPath();
-							ctx.arc(canvas.width / 2 + 150, canvas.height / 2 + 100, 100, 0, Math.PI * 2, true);
-							ctx.closePath();
-							ctx.clip();
-
-							const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
-							ctx.drawImage(avatar, canvas.width / 2 + 50, canvas.height / 2, 200, 200);
-						};
+				if (!state) {
+					var name = member.displayName.toUpperCase();
+					var nameWidth = ctx.measureText(name).width;
+					function fitText(text, size, fontface, fontsize, xPos, yPos) {
+						do {
+							fontsize--;
+							ctx.font = fontsize + 'px ' + fontface;
+						} while (ctx.measureText(text).width > size);
+						ctx.fillText(text, xPos, yPos);
 					}
-				}
-			}
+					if (nameWidth > canvas.width / 2 - 100) {
+						fitText(name, canvas.width / 2 - 100, 'Handwritten', 128, 50, canvas.height * 0.66);
+					} else {
+						ctx.fillText(name, canvas.width / 2 + 50, canvas.height * 0.66);
+					};
+				} else {
+					ctx.beginPath();
+					ctx.arc(canvas.width / 2 + 150, canvas.height / 2 + 100, 100, 0, Math.PI * 2, true);
+					ctx.closePath();
+					ctx.clip();
+
+					const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+					ctx.drawImage(avatar, canvas.width / 2 + 50, canvas.height / 2, 200, 200);
+				};
+			};
+
+
 
 			if (proceed == true) {
 				const background = await Canvas.loadImage('commands/images/kill.jpg');
