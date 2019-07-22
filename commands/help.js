d59e9d94a5bbd73685ac892fb5fe821f1a808a0e
Kaeso <edmier12@gmail.com>
2019-07-22 01:10:16 -0500

    Help

--- commands/help.js
+++ commands/help.js
@@ -8,12 +8,15 @@ module.exports = {
 	execute(message, args) {
 			const data = [];
 			const { commands } = message.client;
-
+			const helpMenu = new Discord.RichEmbed()
 			if (!args.length) {
+				helpMenu.title('Here\'s a list of all the commands:')
+
 				data.push('Here\'s a list of all the commands:');
 				data.push(commands.map(command => command.name).join(', '));
 				data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
+				message.channel.send(data, { split: true });
+				/*
 				return message.author.send(data, { split: true })
 					.then(() => {
 						if (message.channel.type === 'dm') return;
@@ -21,8 +24,9 @@ module.exports = {
 					})
 					.catch(error => {
 						console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-						message.reply('it seems like I can\'t DM you!');
+						message.reply('It seems like I can\'t DM you!');
 					});
+				*/
 			}
 
 			const name = args[0].toLowerCase();
