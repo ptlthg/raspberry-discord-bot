a7c14499696af988d207c6dbd57fc6912702903b
Kaeso <edmier12@gmail.com>
2019-07-21 23:30:16 -0500

    No

--- commands/no.js
+++ commands/no.js
@@ -36,7 +36,7 @@ module.exports = {
 				ctx.fillText(text, xPos, yPos);
 			}
 			if (noWidth > canvas.width - 100) {
-				fitText(name + ' NO', canvas.width / 1.5 - 100, 'Handwritten', 128, 50, canvas.height / 2 + 25);
+				fitText(name + ' NO', canvas.width / 1.5 - 100, 'Handwritten', 128, 30, canvas.height / 2);
 			} else {
 				ctx.fillText(name + ' NO', 50, canvas.height / 8);
 			};
