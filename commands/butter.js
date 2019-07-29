941ebfce5a0713be8d4d2a279185f5f12e3835cd
Kaeso <edmier12@gmail.com>
2019-07-28 22:52:26 -0500

    Butter

--- commands/butter.js
+++ commands/butter.js
@@ -6,16 +6,15 @@ module.exports = {
 	guildOnly: false,
 	execute(message, args) {
     message.channel.send(`
-			**Fahrenheit to Butter:**
-			\`(x°F - 66.2) / 2.34 = °B\`
-			**Celsius to Butter:**
-			\`(x°C - 19) / 1.3 = °B\`
-			**Butter to Fahrenheit:**
-			\`x°B * 2.34 + 66.2 = °F\`
-			**Butter to Celsius:**
-			\`x°B * 1.3 + 19 = °C\`
-
-			Adding 1° Butter is equivalent to adding 2.34°F or 1.3°C
+**Fahrenheit to Butter:**
+\`(x°F - 66.2) / 2.34 = °B\`
+**Celsius to Butter:**
+\`(x°C - 19) / 1.3 = °B\`
+**Butter to Fahrenheit:**
+\`x°B * 2.34 + 66.2 = °F\`
+**Butter to Celsius:**
+\`x°B * 1.3 + 19 = °C\`
+Adding 1° Butter is equivalent to adding 2.34°F or 1.3°C
 		`)
 	},
 };
