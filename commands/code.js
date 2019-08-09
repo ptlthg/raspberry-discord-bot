3498b84c28417c1b23df6c460f2ff99b7032a0eb
Kaeso <edmier12@gmail.com>
2019-08-09 02:08:40 -0500

    Code

--- commands/code.js
+++ commands/code.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const Keyv = require('keyv');
 module.exports = {
 	name: 'code',
 	aliases: ['c', 'decode'],
@@ -6,7 +7,45 @@ module.exports = {
 	usage: 'command plus <number>',
 	guildOnly: false,
 	execute(message, args) {
-		
+		const storage = new Keyv();
+		storage.on('error', err => console.error('Keyv connection error:', err));
+
+		var level = await storage.get('stage');
+		var hint = '';
+		var code = '';
+
+		async function getNext() {
+			const stage = await storage.get('stage');
+
+			if (stage !== null && stage !== undefined) {
+				switch (stage) {
+					case 1:
+						hint = 'Caesar fell down five stairs'
+						code = 'HOH0DYZtHuZsDYDsHOHsDYZtHuDbHeZsIXVsHOZsHOZtDYZuHuZbHODsHODsDYDtHODsDYZuHeZsDYZtHeZtDYZsHuDsDYDuHeZbSB4bHYZrDYVsHYZrDYVsHYZrDYVsHYZbHYZrHNVrHOVsHYZrDYVsHYZbHYZrHOVsDYVsHYZrDYVsHYZrDYVsHYZrDYVsHYZrDYVsHYZ='
+						break;
+					case 2:
+
+						break;
+					case 3:
+
+						break;
+					case 4:
+
+						break;
+					default:
+						break;
+				}
+			} else {
+				storage.set('stage', 0);
+				console.log('It has started');
+				getNext();
+			}
+		};
+
+		if (hint === '') {
+			message.channel.send('Error');
+			return;
+		};
 
 		const response = new Discord.RichEmbed()
 			.setColor('#6b0c19')
