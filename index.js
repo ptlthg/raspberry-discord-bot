f92e4065be352923e7162de59843596cd11756e1
Kaeso <edmier12@gmail.com>
2021-01-24 21:01:55 -0600

    Removed stuff

--- index.js
+++ index.js
@@ -25,43 +25,8 @@ client.on('message', async (message) => {
 	const command =
 		client.commands.get(commandName) ||
 		client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
-	
-	if (message.author.id === '174265140357627904') {
-		if (args[0] !== null && args[0] !== undefined) {
-			const embed = new Discord.RichEmbed()
-				.setColor('#6b0c19')
-				.setAuthor('Admin message')
-				.addField(`From ${message.author.username}:`, message.content.substr(message.content.indexOf(" ") + 1))
-				.setFooter('No, I can\'t read this channel through the bot');
-
-			client.channels.get('578726855104921602').send(embed).catch(collected => {
-				message.channel.send('Channel not found');
-			});
-		}
-	}
 
 	if (!command) return;
-	
-	if (message.author.id === '315455612505030657') {
-		var random = Math.random();
-		let data = 'You really shouldn\'t be able to see this message, but it wouldn\'t suprise me if you broke this again';
-		if (random < 0.15) {
-			data = 'Maybe next time';
-		} else if (random < 0.3) {
-			data = 'Weird that you\'re even trying';
-		} else if (random < 0.45) {
-			data = `Why am I even hosting this for you?`;
-		} else if (random < 0.6) {
-			data = 'Read the room next time';
-		} else if (random < 0.75) {
-			data = 'What makes you think I\'d respond to you?';
-		} else if (random < 0.9) {
-			data = `Sorry I bet you totally wanted to use the ${command.name} command huh`;
-		} else {
-			data = 'You can\'t possibly expect to create a good community while actively banning people who make it up.';
-		}
-		return message.channel.send(data);
-	}
 
 	if (command.guildOnly && message.channel.type !== 'text') {
 		return message.reply("I can't execute that command inside DMs!");
